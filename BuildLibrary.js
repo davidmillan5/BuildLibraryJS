241ac0d451c0d8f348c55b4b2efc6b10e56b3ed7
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-03-02 22:02:08 -0500

    Book Class Created

--- BuildLibrary.js
+++ BuildLibrary.js
@@ -38,3 +38,36 @@ class Media {
     this._ratings.push(newRating);
   }
 }
+
+class Book extends Media {
+  constructor(title, author, pages) {
+    super(title, isCheckedOut, ratings);
+    this._author = author;
+    this._pages = pages;
+  }
+
+  get author() {
+    return this._author;
+  }
+
+  get pages() {
+    return this.pages;
+  }
+
+  getAverageRating() {
+    let ratingsSum = this.ratings.reduce(
+      (currentSum, rating) => currentSum + rating,
+      0
+    );
+    const lengthOfArray = this.ratings.length;
+    return ratingsSum / lengthOfArray;
+  }
+
+  toggleCheckOutStatus() {
+    this._isCheckedOut = !this._isCheckedOut;
+  }
+
+  addRating(newRating) {
+    this._ratings.push(newRating);
+  }
+}
